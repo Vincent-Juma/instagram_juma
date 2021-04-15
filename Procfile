9cafd8839e6a0d20a5bea242d3b1a2aa76a67c42
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-15 23:01:24 +0300

    redeploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn instagram_juma.wsgi --log-file -
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/Instagram_juma/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
